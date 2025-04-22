6c74bc1ce00d64f0d5d9136bc1f87d1401bdb716
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2025-04-22 13:31:39 +0100

    fix: updated underscore

--- pages/game.vue
+++ pages/game.vue
@@ -2,8 +2,7 @@
     <section class="bg-purple-700 min-h-screen">
     <h1 class="text-center font-bold text-white text-7xl py-20">Spelling game</h1>
     <div class="bg-orange-100 rounded-lg max-w-lg mx-auto flex flex-col items-center gap-6 p-10">
-        <h3 class="text-3xl font-bold" >{{ hideText === false ? currentWord.text : '. . .'}}</h3>
-        <h3 class="text-3xl font-bold" >{{ hideText === false ? currentWord.text : currentWord.text.length}}</h3>
+        <h3 class="text-3xl font-bold" >{{ hideText === false ? currentWord.text : '_ '.repeat(currentWord.text.length)}}</h3>
 
         <div
             key="word.id"
